aceb9e65efd835e8d789ae24b9f246e0a05b9b71
Marco De Toma <marco.de.toma@fastned.nl>
2026-01-04 13:00:39 +0100

    chore: fix deprecated removeEnd

--- src/main/java/com/ibanity/apis/client/utils/StringUtils.java
+++ src/main/java/com/ibanity/apis/client/utils/StringUtils.java
@@ -8,26 +8,11 @@ public class StringUtils {
         return Strings.CS.removeEnd(str, remove);
     }
 
-    public static boolean isNotBlank(CharSequence cs) {
+    public static boolean isNotBlank(String cs) {
         return !isBlank(cs);
     }
 
-    public static int length(CharSequence cs) {
-        return cs == null ? 0 : cs.length();
-    }
-
-    public static boolean isBlank(CharSequence cs) {
-        int strLen = length(cs);
-        if (strLen == 0) {
-            return true;
-        } else {
-            for(int i = 0; i < strLen; ++i) {
-                if (!Character.isWhitespace(cs.charAt(i))) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
+    public static boolean isBlank(String cs) {
+        return cs == null || cs.isBlank();
     }
 }
